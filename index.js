02166f5704b8c24c12b083064217113861fc45c8
Vincent Toon <vincenttoon22@gmail.com>
2022-10-04 23:48:39 -0500

    inquirer added for tutor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { seeAllRoles, seeAllEmployees } = require("./data");
 const db = require("./data");
 require("console.table");
 
@@ -66,55 +67,107 @@ const viewAllDepartments = () => {
 };
 
 const viewAllRoles = () => {
-    db.seeAllRoles()
-        .then(([rows]) => {
-            let roles = rows;
-            console.log('\n');
-            console.table(roles);
-        })
-        .then(() => mainMenu());
+  db.seeAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      console.log("\n");
+      console.table(roles);
+    })
+    .then(() => mainMenu());
 };
 
 const viewAllEmployees = () => {
-    db.seeAllEmployees()
-        .then(([rows]) => {
-            let employees = rows;
-            console.log('\n');
-            console.table(employees);
-        })
-        .then(() => mainMenu());
+  db.seeAllEmployees()
+    .then(([rows]) => {
+      let employees = rows;
+      console.log("\n");
+      console.table(employees);
+    })
+    .then(() => mainMenu());
 };
 
 const addDepartment = () => {
-    inquirer
-        .prompt([
-            {
-                name: 'newDepartment',
-                type: 'input',
-                message: 'What is the name of your new department?'
-            }
-        ])
-        .then((response) => {
-            let department = response.newDepartment
-            db.createNewDepartment(department);
-        })
-        .then(() => console.log('Your new department was added to the database!'))
-        .then(() => mainMenu());
+  inquirer
+    .prompt([
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "What is the name of your new department?",
+      },
+    ])
+    .then((response) => {
+      let department = response.newDepartment;
+      db.createNewDepartment(department);
+    })
+    .then(() => console.log("Your new department was added to the database!"))
+    .then(() => mainMenu());
 };
 
+// HERE TO
 const addRole = () => {
-    inquirer
-        .prompt([
-            {
-                name: 'roleName',
-                
-            }
-        ])
+  inquirer.prompt([
+    {
+      name: "roleName",
+      type: "input",
+      message: "Please enter an employment role you would like to add:",
+    },
+    {
+      name: "roleSalary",
+      type: "input",
+      message: "What is the salary of this role?",
+    },
+    {
+      name: "roleDepartment",
+      type: "input",
+      message: "Where would you like to add your new role?",
+    },
+  ]);
 };
 
-const addEmployee = () => {};
+const addEmployee = () => {
+  inquirer.prompt([
+    {
+      name: "add_first_name",
+      type: "input",
+      message: "What is the first name of your new employee?",
+    },
+    {
+      name: "add_last_name",
+      type: "input",
+      message: "What is the last name of your new employee?",
+    },
+    {
+      name: "add_role",
+      type: "list",
+      message: "What is the role of your new employee?",
+      choices: seeAllRoles(),
+    },
+    {
+      name: "add_manager",
+      type: "list",
+      message: "Who is the manager of your new employee?",
+      // choices: 'see all managers?
+    },
+  ]);
+};
 
-const updateEmployee = () => {};
+const updateEmployee = () => {
+  inquirer.prompt([
+    {
+      name: "update",
+      type: "list",
+      message: "Which employee do you want to update?",
+      choices: seeAllEmployees(),
+    },
+    {
+      name: "newRole",
+      type: "list",
+      message: "What is the new role for this employee?",
+      choices: seeAllRoles(),
+    },
+  ]);
+};
+// HERE
 
 const quit = () => {
   console.log("Goodbye!");
